31fbf1e12fe5f634c50ddf3a8b76900ba66a1a71
dnnsjsk <dnnsjsk@gmail.com>
2021-10-24 21:50:45 +0200

    Update streamline-button.spec.ts

--- stencil/src/components/streamline-button/streamline-button.spec.ts
+++ stencil/src/components/streamline-button/streamline-button.spec.ts
@@ -19,10 +19,8 @@ describe('Render button with', function () {
     expect(page.root).toEqualHtml(`
 <streamline-button type="primary">
       <mock:shadow-root>
-        <div class="inline-block relative w-full">
-          <div class="inline-block">
-            <button class="bg-blue-gray-900 bg-transparent border-b border-blue-gray-300 border-none break-words cursor-pointer fill-current flex flex-col focus-dark h-[calc(var(--sl-side-w))] h-[var(--sl-side-w)] hover:bg-[#0e1114] items-center justify-center p-0 text-blue-gray-900 text-center text-white underline-none w-[calc(var(--sl-side-w)-1px)] w-[max-content] w-[var(--sl-side-w)] whitespace-no-wrap" tabindex="0"></button>
-          </div>
+        <div class="flex relative w-full">
+          <button class="bg-blue-gray-900 bg-transparent border-b border-blue-gray-300 border-none break-words cursor-pointer fill-current flex flex-col focus-dark h-[calc(var(--sl-side-w))] h-[var(--sl-side-w)] hover:bg-[#0e1114] items-center justify-center p-0 text-blue-gray-900 text-center text-white underline-none w-[calc(var(--sl-side-w)-1px)] w-[max-content] w-[var(--sl-side-w)] whitespace-no-wrap" tabindex="0"></button>
         </div>
       </mock:shadow-root>
     </streamline-button>
@@ -41,14 +39,12 @@ describe('Render button with', function () {
       expect(page.root).toEqualHtml(`
 <streamline-button icon="menu" type="sidebar">
       <mock:shadow-root>
-        <div class="inline-block relative w-full">
-          <div class="inline-block">
-            <button class="!content-center !grid !justify-items-center bg-transparent border-b border-blue-gray-300 break-words cursor-pointer false flex flex-col focus h-[calc(var(--sl-side-w))] hover:bg-blue-gray-100 hover:text-blue-gray-900 items-center justify-center p-0 sm:!grid-rows-[20px,20px] text-blue-gray-500 text-blue-gray-900 text-center underline-none w-[calc(var(--sl-side-w)-1px)] w-[max-content] whitespace-no-wrap" tabindex="0" style="border-bottom: 1px solid rgba(209,213,219,var(--tw-border-opacity));">
-              <svg class="fill-current h-[18px]" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                <path d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z" fill="currentColor"></path>
-              </svg>
-            </button>
-          </div>
+        <div class="flex relative w-full">
+          <button class="!content-center !grid !justify-items-center bg-transparent border-b border-blue-gray-300 break-words cursor-pointer false flex flex-col focus h-[calc(var(--sl-side-w))] hover:bg-blue-gray-100 hover:text-blue-gray-900 items-center justify-center p-0 sm:!grid-rows-[20px,20px] text-blue-gray-500 text-blue-gray-900 text-center underline-none w-[calc(var(--sl-side-w)-1px)] w-[max-content] whitespace-no-wrap" tabindex="0" style="border-bottom: 1px solid rgba(209,213,219,var(--tw-border-opacity));">
+            <svg class="fill-current h-[18px]" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+              <path d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z" fill="currentColor"></path>
+            </svg>
+          </button>
         </div>
       </mock:shadow-root>
     </streamline-button>
@@ -66,14 +62,12 @@ describe('Render button with', function () {
       expect(page.root).toEqualHtml(`
 <streamline-button icon="menu" type="sidebar">
       <mock:shadow-root>
-        <div class="inline-block relative w-full">
-          <div class="inline-block">
-            <button class="!content-center !grid !justify-items-center -right-px bg-transparent bg-white border-b border-blue-gray-300 border-r border-white break-words cursor-pointer flex flex-col focus h-[calc(var(--sl-side-w))] hover:!bg-white hover:bg-blue-gray-100 hover:text-blue-gray-900 items-center justify-center p-0 pointer-events-none relative sm:!grid-rows-[20px,20px] text-blue-700 text-blue-gray-500 text-blue-gray-900 text-center underline-none w-[calc(var(--sl-side-w)-1px)] w-[max-content] whitespace-no-wrap" tabindex="-1" style="border-bottom: 1px solid rgba(209,213,219,var(--tw-border-opacity));">
-              <svg class="fill-current h-[18px]" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                <path d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z" fill="currentColor"></path>
-              </svg>
-            </button>
-          </div>
+        <div class="flex relative w-full">
+          <button class="!content-center !grid !justify-items-center -right-px bg-transparent bg-white border-b border-blue-gray-300 border-r border-white break-words cursor-pointer flex flex-col focus h-[calc(var(--sl-side-w))] hover:!bg-white hover:bg-blue-gray-100 hover:text-blue-gray-900 items-center justify-center p-0 pointer-events-none relative sm:!grid-rows-[20px,20px] text-blue-700 text-blue-gray-500 text-blue-gray-900 text-center underline-none w-[calc(var(--sl-side-w)-1px)] w-[max-content] whitespace-no-wrap" tabindex="-1" style="border-bottom: 1px solid rgba(209,213,219,var(--tw-border-opacity));">
+            <svg class="fill-current h-[18px]" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+              <path d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z" fill="currentColor"></path>
+            </svg>
+          </button>
         </div>
       </mock:shadow-root>
     </streamline-button>
@@ -94,10 +88,8 @@ describe('Render button with', function () {
       expect(page.root).toEqualHtml(`
 <streamline-button type="main">
       <mock:shadow-root>
-        <div class="inline-block relative w-full">
-          <div class="inline-block">
-            <button class="bg-blue-gray-50 border border-blue-gray-200 break-words cursor-pointer focus hover:border-blue-600 leading-none min-w-[75px] px-3 py-2.5 text-blue-600 text-center text-sm underline-none w-[max-content] whitespace-no-wrap" tabindex="0" style="border-bottom: 1px solid rgba(209,213,219,var(--tw-border-opacity));"></button>
-          </div>
+        <div class="flex relative w-full">
+          <button class="bg-blue-gray-50 border border-blue-gray-200 break-words cursor-pointer focus hover:border-blue-600 leading-none px-3 py-2.5 text-blue-600 text-center text-sm underline-none w-[max-content] whitespace-no-wrap" tabindex="0" style="border-bottom: 1px solid rgba(209,213,219,var(--tw-border-opacity));"></button>
           <div class="!grid auto-cols-max divide-blue-gray-200 divide-x grid-flow-col" style="display: block;">
             <button class="border-none focus-dark">
               <span class="flex h-8 items-center justify-center w-8">
@@ -124,17 +116,15 @@ describe('Render button with', function () {
       expect(page.root).toEqualHtml(`
 <streamline-button favourite="" type="main">
       <mock:shadow-root>
-        <div class="inline-block relative w-full">
-          <div class="inline-block">
-            <button class="bg-blue-gray-50 border border-blue-gray-200 break-words cursor-pointer focus hover:border-blue-600 leading-none min-w-[75px] px-3 py-2.5 text-blue-600 text-center text-sm underline-none w-[max-content] whitespace-no-wrap" tabindex="0" style="border-bottom: 1px solid rgba(209,213,219,var(--tw-border-opacity));"></button>
-            <span class="-right-1 -top-3 absolute bg-white border border-blue-gray-200 flex h-4 items-center justify-center pointer-events-none rounded-full text-red-500 w-4">
-              <span class="flex h-full items-center justify-center w-full">
-                <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" style="width: 8px; height: 8px;">
-                  <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z" fill="currentColor"></path>
-                </svg>
-              </span>
+        <div class="flex relative w-full">
+          <button class="bg-blue-gray-50 border border-blue-gray-200 break-words cursor-pointer focus hover:border-blue-600 leading-none px-3 py-2.5 text-blue-600 text-center text-sm underline-none w-[max-content] whitespace-no-wrap" tabindex="0" style="border-bottom: 1px solid rgba(209,213,219,var(--tw-border-opacity));"></button>
+          <span class="-right-1.5 -top-1.5 absolute bg-white border border-blue-gray-200 flex h-4 items-center justify-center pointer-events-none rounded-full text-red-500 w-4">
+            <span class="flex h-full items-center justify-center w-full">
+              <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" style="width: 8px; height: 8px;">
+                <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z" fill="currentColor"></path>
+              </svg>
             </span>
-          </div>
+          </span>
           <div class="!grid auto-cols-max divide-blue-gray-200 divide-x grid-flow-col" style="display: block;">
             <button class="border-none focus-dark">
               <span class="flex h-8 items-center justify-center text-red-500 w-8">
